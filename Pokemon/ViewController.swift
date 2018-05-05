698824e536b964f0317343e68883042410a49472
atsushi130 <atsushi_main@outlook.jp>
2018-05-05 23:43:01 +0900

    implement: smaple parse code

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let parser = Parser<Plist>()
+        parser.delegate = self
+        parser.parse(resource: .pokemon)
+        parser.parse(resource: .move)
+        parser.parse(resource: .ability)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: ParserDelegate {
+    func didLoad(items: [Item]) {
+        print(items)
+    }
+}
