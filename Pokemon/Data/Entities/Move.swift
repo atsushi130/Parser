abaf19f769c6caf7ffa2cbc249d26e9c27d8c41a
atsushi130 <atsushi_main@outlook.jp>
2018-05-06 15:22:50 +0900

    add: Move entity

---
+++ Pokemon/Data/Entities/Move.swift
@@ -0,0 +1,95 @@
+//
+// Created by Atsushi Miyake on 2018/05/06.
+// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+import  RealmSwift
+
+public final class Move: Object {
+
+    @objc dynamic var id = 0
+    @objc dynamic var name = ""
+    @objc dynamic var accuracy = 0
+    @objc dynamic var additionEffect: String? = "" // 型 AdditionEffect にしたい (やけど とか)
+    @objc dynamic var additionProbability: Double = 0.0
+    @objc dynamic var category = "" // 型を Category にしたい
+    @objc dynamic var despondency: Double = 0.0
+    @objc dynamic var direct = true
+    @objc dynamic var information = ""
+    @objc dynamic var magicCoat = true
+    @objc dynamic var parroting = true
+    @objc dynamic var power = 0
+    @objc dynamic var pp = 0
+    @objc dynamic var priority = 0
+    @objc dynamic var protect = true
+    @objc dynamic var steal = true
+    @objc dynamic var target = "" // 型を MoveTarget にしたい
+    @objc dynamic var type = "" // 型を Type にしたい
+    @objc dynamic var vital: Bool = true
+
+    public convenience init(move: [String: Any]) {
+        self.init()
+        let id = Int((move["id"] as! String).replacingOccurrences(of:" ", with:""))!
+        self.id = id
+        self.name = move["name"] as! String
+        self.accuracy = Int(move["accuracy"] as! String) ?? 0
+
+        let additionEffect: String?
+        if let additionEffect = move["additionEffect"] as? String {
+            if additionEffect == "-" {
+                self.additionEffect = nil
+            } else {
+                self.additionEffect = additionEffect
+            }
+        } else {
+            self.additionEffect = nil
+        }
+
+        if let additionProbability = move["additionProbabiliry"] as? String {
+            if additionProbability != "-" {
+                let value = Double(additionProbability.replacingOccurrences(of:"%", with:""))!
+                self.additionProbability = value/100
+            }
+        }
+
+        self.category = move["category"] as! String
+
+        if let despondency = move["despondency"] as? String {
+            if despondency != "-" {
+                let value = Double(despondency.replacingOccurrences(of:"%", with:""))!
+                self.despondency = value/100
+            }
+        }
+
+        self.direct = Bool(move["direct"] as! String)!
+        self.information = move["information"] as! String
+        self.magicCoat = Bool(move["magicCoat"] as! String)!
+        self.parroting = Bool(move["parroting"] as! String)!
+        self.power = Int(move["power"] as! String) ?? 0
+        self.pp = Int(move["pp"] as! String)!
+        self.priority = Int(move["priority"] as! String)!
+        self.protect = Bool(move["protect"] as! String)!
+        self.steal = Bool(move["steal"] as! String)!
+        self.target = move["target"] as! String
+        self.type = move["type"] as! String
+
+        if let vital = move["vital"] as? String {
+            if vital == "-" {
+                self.vital = false
+            } else {
+                self.vital = Bool(vital)!
+            }
+        } else {
+            self.vital = false
+        }
+    }
+
+    public override static func primaryKey() -> String? {
+        return "id"
+    }
+
+    public static var repository: Repository<Move> {
+        return Repository<Move>()
+    }
+}
