ee4f4b673b70c6b963177b2e2a4eb17f91e6dfcb
atsushi130 <atsushi_main@outlook.jp>
2018-05-06 23:33:25 +0900

    add: addition effect repository

---
+++ Pokemon/Data/Repository/AdditionEffectRepository.swift
@@ -0,0 +1,18 @@
+//
+// Created by Atsushi Miyake on 2018/05/06.
+// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+extension Repository where ObjectType: AdditionEffect {
+
+    public static var repository: Repository<ObjectType> {
+        return Repository<ObjectType>()
+    }
+
+    public func findBy(name: String) -> ObjectType? {
+        let codePredicate = NSPredicate(format: "name == %@", name)
+        return Repository<ObjectType>.repository.findAll(predicate: codePredicate).first
+    }
+}
