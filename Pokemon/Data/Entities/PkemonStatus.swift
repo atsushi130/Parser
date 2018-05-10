07b79dca8ecb5b6b13cd763e0dd7adbee7dce0c4
atsushi130 <atsushi_main@outlook.jp>
2018-05-10 21:57:17 +0900

    feature: pokedex has one pokemon status

---
+++ Pokemon/Data/Entities/PkemonStatus.swift
@@ -0,0 +1,15 @@
+//
+// Created by Atsushi Miyake on 2018/05/08.
+// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public struct PokemonStatus {
+    public let attack: Int
+    public let defence: Int
+    public let spAttack: Int
+    public let spDefence: Int
+    public let speed: Int
+    public let hp: Double
+}
