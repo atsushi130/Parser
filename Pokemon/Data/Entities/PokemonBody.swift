58441cf59060a0c8de8250e720f984f1dff2160a
atsushi130 <atsushi_main@outlook.jp>
2018-05-10 21:57:32 +0900

    feature: pokedex has one pokemon body

---
+++ Pokemon/Data/Entities/PokemonBody.swift
@@ -0,0 +1,11 @@
+//
+// Created by Atsushi Miyake on 2018/05/08.
+// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public struct PokemonBody {
+    let weight: Int
+    let height: Int
+}
