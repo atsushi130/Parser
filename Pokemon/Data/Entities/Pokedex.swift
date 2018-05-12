1e7b67fb849dc9f4f5ada3f47e5f41211a606652
atsushi130 <atsushi_main@outlook.jp>
2018-05-12 20:14:32 +0900

    add: gif image resources and evolution information

--- Pokemon/Data/Entities/Pokedex.swift
+++ Pokemon/Data/Entities/Pokedex.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import RealmSwift
+import UIKit
+import Gifu
 
 public final class Pokedex: Object {
     
@@ -16,26 +18,35 @@ public final class Pokedex: Object {
     public var types = List<Type>()
     public var moves = List<Move>()
     public var abilities = List<Ability>()
-
-    // 個体値
+    @objc private dynamic var frontImageData: Data!
+    @objc private dynamic var backImageData: Data!
     @objc private dynamic var hp        = 0.0
     @objc private dynamic var attack    = 0
     @objc private dynamic var defence   = 0
     @objc private dynamic var spAttack  = 0
     @objc private dynamic var spDefence = 0
     @objc private dynamic var speed     = 0
-
     @objc private dynamic var height = 0
     @objc private dynamic var weight = 0
-
     @objc public dynamic var exp = 0
+    private let evolutionLevel = RealmOptional<Int>()
+    @objc private var evolutionMethod: String? = nil
+    private let evolutionPokemonId = RealmOptional<Int>()
 
     public var baseStatus: PokemonStatus {
         return PokemonStatus(attack: self.attack, defence: self.defence, spAttack: self.spAttack, spDefence: self.spDefence, speed: self.speed, hp: self.hp)
     }
+    
+    public var evolutionPokemon: Evolution? {
+        return Evolution(level: self.evolutionLevel.value, method: self.evolutionMethod, pokemonId: self.evolutionPokemonId.value)
+    }
 
     public var body: PokemonBody {
         return PokemonBody(weight: self.weight, height: self.height)
+    }
+    
+    public var frontImageView: GIFImageView {
+        return GIFImageView(data: self.frontImageData)
     }
 
     public convenience init(pokemon: [String: Any]) {
@@ -51,11 +62,26 @@ public final class Pokedex: Object {
         self.spAttack  = pokemon["sp_atk"] as! Int
         self.spDefence = pokemon["sp_def"] as! Int
         self.speed     = pokemon["spd"] as! Int
+        self.exp       = pokemon["exp"] as! Int
+        self.height    = Int(pokemon["height"] as! String)!
+        self.weight    = Int(pokemon["weight"] as! String)!
+        
+        let evolution = pokemon["evolution"] as? [String: Any]
+        self.evolutionLevel.value = evolution?["level"] as? Int
+        self.evolutionMethod = evolution?["method"] as? String
+        if let evolutionPokemonName = evolution?["to"] as? String {
+            self.evolutionPokemonId.value = Pokedex.repository.findBy(name: evolutionPokemonName)?.id
+        }
 
-        self.exp = pokemon["exp"] as! Int
-
-        self.height = Int(pokemon["height"] as! String)!
-        self.weight = Int(pokemon["weight"] as! String)!
+        let frontImageName = pokemon["english_name"] as! String
+        let backImageName  = pokemon["english_name"] as! String + "-back"
+        let bundlePath = Bundle.main.path(forResource: "images", ofType: ".bundle")!
+        let frontPath  = Bundle(path: bundlePath)!.path(forResource: "frontSpite/\(frontImageName)", ofType: ".gif")
+        let backPath   = Bundle(path: bundlePath)!.path(forResource: "backSplite/\(backImageName)", ofType: ".gif")
+        let frontUrl = URL(fileURLWithPath: frontPath!)
+        let backUrl  = URL(fileURLWithPath: backPath!)
+        self.frontImageData = try! Data(contentsOf: frontUrl)
+        self.backImageData  = try! Data(contentsOf: backUrl)
 
         let types = pokemon["types"] as! [String]
         types.forEach { type in
@@ -125,3 +151,10 @@ public final class Pokedex: Object {
     }
 }
 
+extension GIFImageView {
+    convenience init(data: Data) {
+        let image = UIImage(data: data)
+        self.init(image: image)
+        self.animate(withGIFData: data)
+    }
+}
